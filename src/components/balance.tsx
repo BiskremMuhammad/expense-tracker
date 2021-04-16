d5612a32abb1b4a5692db37dbebe3e57c21dabdc
Muhammad Omran <use.yurMind@gmail.com>
2021-04-16 18:01:31 +0200

    build the functionality of the balance component

--- src/components/balance.tsx
+++ src/components/balance.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContextState, ExpensesContext } from "../context/context";
+import { Expense } from "../types/types";
 
 export const Balance = () => {
+  const { expenses } = useContext<ContextState>(ExpensesContext);
+  const balance: number = !expenses.length
+    ? 0
+    : expenses.length === 1
+    ? expenses[0].amount
+    : expenses.reduce<number>(
+        (a, b) =>
+          typeof a === "number" ? a : (a as Expense).amount + b.amount,
+        0
+      );
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">${balance.toFixed(2)}</h1>
     </>
   );
 };
