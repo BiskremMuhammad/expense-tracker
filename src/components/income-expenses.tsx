9669abe85680e0973009ec204f97c4d158e45e7b
Muhammad Omran <use.yurMind@gmail.com>
2021-04-16 18:05:46 +0200

    build the functionality of the income-expense component

--- src/components/income-expenses.tsx
+++ src/components/income-expenses.tsx
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { ContextState, ExpensesContext } from "../context/context";
+import { Expense } from "../types/types";
 
 export const IncomeExpenses = () => {
+  const { expenses } = useContext<ContextState>(ExpensesContext);
+  const income: number = !expenses.length
+    ? 0
+    : expenses.length === 1
+    ? expenses[0].amount > 0
+      ? expenses[0].amount
+      : 0
+    : expenses.reduce<number>(
+        (a, b) =>
+          typeof a === "number"
+            ? a
+            : (a as Expense).amount + b.amount > 0
+            ? b.amount
+            : 0,
+        0
+      );
+
+  const expense: number = !expenses.length
+    ? 0
+    : expenses.length === 1
+    ? expenses[0].amount < 0
+      ? expenses[0].amount
+      : 0
+    : expenses.reduce<number>(
+        (a, b) =>
+          typeof a === "number"
+            ? a
+            : (a as Expense).amount + b.amount < 0
+            ? b.amount
+            : 0,
+        0
+      );
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          +$0.00
+          +${income.toFixed(2)}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id="money-minus" className="money minus">
-          -$0.00
+          -${(expense * -1).toFixed(2)}
         </p>
       </div>
     </div>
