b87ef3469cc369ec222920b37cd51dbe8342ce1b
Muhammad Omran <use.yurMind@gmail.com>
2021-04-16 17:36:44 +0200

    add the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useContext } from "react";
+
+import { Header } from "./components/header";
 import { ContextState, ExpensesContext } from "./context/context";
-import { Expense } from "./types/types";
 
 function App() {
   const expenses = useContext<ContextState>(ExpensesContext);
 
   return (
     <ExpensesContext.Provider value={expenses}>
-      <div className="App"></div>
+      <div className="container">
+        <Header />
+      </div>
     </ExpensesContext.Provider>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Header = () => <h1>Expenses Tracker</h1>;
