2dda92708cefd82b30626b23258c6eec1e8cb609
Muhammad Omran <use.yurMind@gmail.com>
2021-04-17 15:08:56 +0200

    add the functionality to transactions component

--- src/components/transactions.tsx
+++ src/components/transactions.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContextState, ExpensesContext } from "../context/context";
+import { ActionTypes } from "../reducer/action-types";
+import { Expense } from "../types/types";
 
 export const Transactions = () => {
+  const { expenses, dispatch } = useContext<ContextState>(ExpensesContext);
+
+  const removeTransaction = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    itemId: string
+  ) => {
+    e.preventDefault();
+
+    dispatch({
+      type: ActionTypes.DELETE_EXPENSE,
+      payload: itemId,
+    });
+  };
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {expenses.map((e: Expense, i: number) => (
+          <li className={e.amount < 0 ? "minus" : "plus"}>
+            {e.description}{" "}
+            <span>
+              {e.amount < 0 ? "-" : ""}${Math.abs(e.amount).toFixed(2)}
+            </span>
+            <button
+              className="delete-btn"
+              onClick={(event) => removeTransaction(event, e.id)}
+            >
+              x
+            </button>
+          </li>
+        ))}
       </ul>
     </>
   );
